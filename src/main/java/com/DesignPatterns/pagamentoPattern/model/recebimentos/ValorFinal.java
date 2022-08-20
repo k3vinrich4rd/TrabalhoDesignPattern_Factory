497b975a5df8e99953c09383ab3da5a42fed0a50
k3vinrich4rd <kevinrichardve@gmail.com>
2022-08-19 21:53:33 -0300

    Inserindo novas atualizações no projeto

--- src/main/java/com/DesignPatterns/pagamentoPattern/model/recebimentos/ValorFinal.java
+++
@@ -1,9 +0,0 @@
-package com.DesignPatterns.pagamentoPattern.model.recebimentos;
-
-import java.math.BigDecimal;
-
-public class ValorFinal {
-    public static BigDecimal valorFinal(BigDecimal valorAPagar, BigDecimal valorDesconto){
-        return valorAPagar.subtract(valorDesconto);
-    }
-}
